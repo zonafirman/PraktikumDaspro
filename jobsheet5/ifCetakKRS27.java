7a6b11224e766adde14679bf90c5d0002e90c44a
Zona-firman <zonafirman2@gmail.com>
2025-10-02 10:31:33 +0700

    Tugas 2

--- jobsheet5/ifCetakKRS27.java
+++ jobsheet5/ifCetakKRS27.java
@@ -1,16 +1,21 @@
 package PraktikumDaspro.jobsheet5;
 
 import java.util.Scanner;
-public class ifCetakKRS27{ public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.print("Apakah UKT sudah lunas? (true/false): ");
-        boolean uktLunas = sc.nextBoolean();
-        String pesanStatus = uktLunas ? "Pembayaran UKT terverifikasi" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
-        System.out.println(pesanStatus);
-        if (uktLunas) {
-            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+public class ifCetakKRS27{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
         }
 
+        input.close();
     }
-    }
+}
+
+    
