a26479acfa2d72a6566414ee92366a117ba1cc57
Zona-firman <zonafirman2@gmail.com>
2025-10-02 08:30:02 +0700

    Percobaan 1

---
+++ jobsheet5/ifCetakKRS27.java
@@ -0,0 +1,15 @@
+package PraktikumDaspro.jobsheet5;
+import java.util.Scanner;
+public class ifCetakKRS27{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        }
+    }
+}
